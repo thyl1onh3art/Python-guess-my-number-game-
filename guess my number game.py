8349b4b761b23ce8df7fc56058a5082f84aee4c2
thyl1onh3art <84870139+thyl1onh3art@users.noreply.github.com>
2024-11-30 22:16:22 +0000

    Add files via upload

---
+++ guess my number game.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print('Hello, welocme to my guessing game')
+
+ready_to_play = input('Press y to play and n to exit' ' ')
+
+if ready_to_play == 'n':
+    print('Goodbye')
+else:
+    print('lets play')
